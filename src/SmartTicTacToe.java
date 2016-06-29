c561160b0c5b88e2b24d1a837bb9356868355bd1
ak11234 <akpc556@gmail.com>
2016-06-29 16:35:19 -0500

    Smart...er computer

--- src/SmartTicTacToe.java
+++ src/SmartTicTacToe.java
@@ -1,86 +1,120 @@
-import javax.sql.rowset.spi.SyncResolver;
 import java.util.Scanner;
 
 /**
  * Created by Adam on 6/29/2016. Worksheet 4
  */
-public class TicTacToe {
-    public static final String[] playerSymbol = { "X", "O" };
-    public static String[] playerType = {"Human" , "Computer"};
-    public static void main(String[] args){
+public class SmartTicTacToe {
+    public static final String[] playerSymbol = {"X", "O"};
+    public static String[] playerType = {"Human", "Computer"};
+
+    public static void main(String[] args) {
         System.out.println("How many players?: ");
         Scanner scan = new Scanner(System.in);
         int numplayers;
         numplayers = scan.nextInt();
-        if (numplayers==0){
-            playerType[0]="Computer";
+        if (numplayers == 0) {
+            playerType[0] = "Computer";
         }
-        if (numplayers==2){
-            playerType[1]="Human";
+        if (numplayers == 2) {
+            playerType[1] = "Human";
         }
         String[] board = new String[9];
         int player = 0;
         int currentMove;
         clearBoard(board);
-        while (getBoardState(board)==4){
+        while (getBoardState(board) == 4) {
             //GAME TIME
             printBoard(board);
-            currentMove=getMove(board,player);
-            while (! isLegalMove(board, currentMove)){
+            currentMove = getMove(board, player);
+            while (!isLegalMove(board, currentMove)) {
                 System.out.println(playerType[player] + " has made an invalid move and must try again.");
-                currentMove=getMove(board,player);
+                currentMove = getMove(board, player);
             }
-            board[currentMove]=playerSymbol[player];
+            board[currentMove] = playerSymbol[player];
             player = switchPlayer(player);
         }
         printBoard(board);
-        int status =getBoardState(board);
-        if (status==1){
+        int status = getBoardState(board);
+        if (status == 1) {
             System.out.println("X Wins!");
-        }
-        else if (status==2){
+        } else if (status == 2) {
             System.out.println("O Wins!");
-        }
-        else if (status==3){
+        } else if (status == 3) {
             System.out.println("Looks like it was a tie -_-");
         } else {
             System.out.println("Error: State Code: " + status);
         }
         System.out.println("State code: " + getBoardState(board));
     }
-    public static int getMove(String[] board, int player){
-        if (playerType[player].equals("Human")){
+
+    public static int getMove(String[] board, int player) {
+        if (playerType[player].equals("Human")) {
             return getHumanMove();
-        }
-        else if (playerType[player].equals("Computer")){
-            return getComputerMove(board,player);
+        } else if (playerType[player].equals("Computer")) {
+            return getComputerMove(board, player);
         }
         return -1;
     }
-    public static boolean isLegalMove(String[] board, int move){
-        if (board[move].equals("*")){
+
+    public static boolean isLegalMove(String[] board, int move) {
+        if (board[move].equals("*")) {
             return true;
         }
         return false;
     }
-    public static int switchPlayer(int player){
-        if (player==0){
+
+    public static int switchPlayer(int player) {
+        if (player == 0) {
             return 1;
-        }else {
+        } else {
             return 0;
         }
     }
-    public static int getHumanMove(){
+
+    public static int getHumanMove() {
         System.out.println("Present your move: ");
         Scanner scan = new Scanner(System.in);
         int num;
         num = scan.nextInt();
         return num;
     }
-    public static int getComputerMove(String[] board, int computernum){
-        return ((int) (Math.random()*9));
-    //    board[move]=playerSymbol[computernum];
-    //    System.out.println("i moved -" + playerSymbol[computernum] + "- into position " + move);
+
+    public static int getComputerMove(String[] board, int player) {
+        String myValue;
+        String otherValue;
+        if (playerSymbol[player].equals("X")) {
+            myValue = "X"; // The computer is X
+            otherValue = "O"; // The other player is O
+        } else {
+            myValue = "O"; // The computer is O
+            otherValue = "X"; // The other player is X
+        }
+        /*
+        Check the 0 square (upper left corner)
+
+        If the 0 square is empty,
+        Check if the 1 and 2 squares have my pieces in them.
+        If so, 0 is a good move to make!
+
+        Check if the 3 and 6 squares have my pieces in them.
+        If so, 0 is a good move to make!
+
+        Check if the 4 and 8 squares have my pieces in them.
+        If so, 0 is a good move to make!
+        */
+        if (board[0].equals("*")) {
+            if (board[1].equals(myValue) && board[2].equals(myValue)) {
+                return 0;
+            }
+            if (board[3].equals(myValue) && board[6].equals(myValue)) {
+                return 0;
+            }
+
+            if (board[4].equals(myValue) && board[8].equals(myValue)) {
+                return 0;
+            }
+        }
+        return (int) (Math.random() * 9);
     }
     public static void printRow(String[] board, int startSquare)
     {
