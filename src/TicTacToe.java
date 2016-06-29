4bbd3365693cf0dc9c80e79f2244a813ccfef75f
ak11234 <akpc556@gmail.com>
2016-06-29 13:27:57 -0500

    Starting getBoardState

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -12,10 +12,6 @@ public class TicTacToe {
         board[4] = "O";
         board[7] = "O";
         printBoard(board);
-        board[0] = "X";
-        printBoard(board);
-        clearBoard(board);
-        printBoard(board);
     }
     public static void printRow(String[] board, int startSquare)
     {
@@ -38,5 +34,28 @@ public class TicTacToe {
         for (int k=0 ; k<=8 ; k++){
             board[k]="*";
         }
+    }
+    public static int getBoardState(String[] board){
+        int state=5;
+        for (int k=0 ; k<=6 ; k+=3){ //check for horizontal row wins
+            if (board[k].equals(board[k+1])&&board[k].equals(board[k+2])){
+                if (board[k].equals("X")){
+                    state=1;//x wins
+                } else if (board[k].equals("O")){
+                    state=2; //o wins
+                }
+            }
+        }
+        for (int k=0 ; k<=3 ; k++){ //check for vertical wins
+            if (board[k].equals(board[k+3])&&board[k].equals(board[k+6])){
+                if (board[k].equals("X")){
+                    state=1;//x wins
+                } else if (board[k].equals("O")){
+                    state=2; //o wins
+                }
+            }
+        }
+
+        return state;
     }
 }
